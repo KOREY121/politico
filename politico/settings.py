8477398f979c7bc6949a3ed90cbd5462c53772f7
korey <olorunfemioluwakorede9@gmail.com>
2026-03-19 10:42:58 +0100

    fix: allowed_host config

--- politico/settings.py
+++ politico/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = 'django-insecure-^&##%p_5&e5!szsh%5j3z6zmz$yw47ofdmvx)phgp^v_pdfp@$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='localhost,127.0.0.1'
+).split(',')
 
 
 # Application definition
