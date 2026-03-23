f73aac55556cb7ebba0c2ea2d601e11422c13515
korey <olorunfemioluwakorede9@gmail.com>
2026-03-23 12:24:27 +0100

    temp: allow all CORS

--- politico/settings.py
+++ politico/settings.py
@@ -29,6 +29,8 @@ SECRET_KEY = 'django-insecure-^&##%p_5&e5!szsh%5j3z6zmz$yw47ofdmvx)phgp^v_pdfp@$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = config(
     'CORS_ALLOWED_ORIGINS',
     default='http://localhost:5173,http://127.0.0.1:5173'
